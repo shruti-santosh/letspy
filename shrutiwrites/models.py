1fc76911806fbab19b6c64f26c6e428dd4169925
shruti-santosh <shrutisantosh99/2yahoo.com>
2019-11-03 02:45:19 +0530

    changes in html

---
+++ shrutiwrites/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=60)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    
+    def __str__(self):
+        return self.title
+# Create your models here.
